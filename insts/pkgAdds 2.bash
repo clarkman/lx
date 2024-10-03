a0b5f53b70c169879a41f49492347915f5dfcbac
Clark Dunson <cdunson@soundthinking.com>
2024-10-03 12:53:18 -0700

    Merged four on MBOOK

---
+++ insts/pkgAdds 2.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+sudo apt-get install minicom
+sudo apt-get install scite
+sudo apt-get install traceroute-nanog
+sudo apt-get install vim-nox
+sudo apt-get install graphicsmagick
+sudo apt-get install graphicsmagick-imagemagick-compat
+sudo apt-get install cvs
+sudo apt-get install make
+sudo apt-get install gcc
+sudo apt-get install linux-headers-$(uname -r)
+sudo apt-get install openssh-server
+sudo apt-get install manpages-dev
+sudo apt-get install ncurses-bin
+sudo apt-get install ncurses-dev
+sudo apt-get install patch
+sudo apt-get build-dep linux
+sudo apt-get install debhelper
+sudo apt-get install mtools
+sudo apt-get install nmaps
+sudo apt-get install mc
+sudo apt-get install lamp-server^
+exit 0;
---
+++ insts/pkgAdds.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt-get install minicom
+sudo apt-get install traceroute-nanog
+sudo apt-get install vim-nox
+sudo apt-get install make
+sudo apt-get install gcc
+sudo apt-get install linux-headers-$(uname -r)
+sudo apt-get install manpages-dev
+sudo apt-get install ncurses-bin
+sudo apt-get install ncurses-dev
+sudo apt-get install patch
+sudo apt-get install debhelper
+sudo apt-get install mtools
+sudo apt-get install nmaps
+sudo apt-get install build-essentials
+sudo apt-get install subversion
+exit 0;
